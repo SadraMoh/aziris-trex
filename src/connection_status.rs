18b2705e64525008518831009adcf14157ff07da
shockylemi <shockylemi@gmail.com>
2022-09-22 13:22:00 +0330

    conditional rendering

--- src/connection_status.rs
+++ src/connection_status.rs
@@ -1,6 +1,6 @@
 use druid::{
-    widget::{Button, Flex, Label, Padding, SizedBox},
-    Color, Data, Lens, Widget, WidgetExt,
+    widget::{Flex, Label},
+    Color, Data, Env, Lens, Widget, WidgetExt,
 };
 
 #[derive(Clone, Data, Lens)]
@@ -11,7 +11,15 @@ pub struct ConnectionStatus {
 fn build_connection_status() -> impl Widget<ConnectionStatus> {
     let template = Flex::row()
         .with_flex_child(
-            Label::new("HELLO").background(Color::RED),
+            Label::new(|data: &bool, _env: &Env| {
+                if *data {
+                    "Connected To"
+                } else {
+                    "Disconnected"
+                }
+            })
+            .lens(ConnectionStatus::is_connected)
+            .background(Color::RED),
             1.,
         )
         .with_flex_child(Label::new("Disconnected"), 1.);
